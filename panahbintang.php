cd9b14a3c99c80c1d111c4428c3800335b4dc23c
atthoriqi <thoriq1997@gmail.com>
2016-07-22 09:06:25 +0700

    coba

---
+++ panahbintang.php
@@ -0,0 +1,20 @@
+<?php
+for($a=0; $a<5; $a++)
+{
+for($b=10; $a<$b; $b--)
+{
+  echo "&nbsp";
+}
+  echo"****<br>";
+}
+
+for( $a=5;$a>0;$a--)
+{
+for($b=10;$b>$a;$b--)
+{
+echo "&nbsp";
+}
+echo"****<br>";
+}
+
+ ?>
